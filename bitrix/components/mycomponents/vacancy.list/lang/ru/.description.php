2ada113818200e5fb5f636449fb5f2aefdf037dd
ASDerevyanko <ASDerevyanko@1cbit.ru>
2020-12-28 15:12:52 +0300

    default vacancy_list

---
+++ bitrix/components/mycomponents/vacancy.list/lang/ru/.description.php
@@ -0,0 +1,6 @@
+<?
+$MESS ['T_IBLOCK_DESC_LIST'] = "Список вакансий";
+$MESS ['T_IBLOCK_DESC_LIST_DESC'] = "Список вакансий из одного информационного блока.";
+$MESS ['T_IBLOCK_DESC_VACANCY'] = "Вакансии";
+$MESS ['T_IBLOCK_EXTRA'] = "Дополнительно";
+?>
